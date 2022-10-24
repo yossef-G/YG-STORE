22585f65e2dbcea32963f4b3d0c98a78d29c5a1d
yossef-G <yossef.g.09@gmail.com>
2022-10-24 14:32:02 +0300

    CREATING - YG-STORE

---
+++ my-store/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div >
+      <header>
+        <a href="/">YG-STORE</a>
+      </header>
+      <main>products</main>
+    </div>
+  );
+}
+
+export default App;
